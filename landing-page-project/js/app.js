f96a074924d848dacfc47ca67d8e684db816effd
Ahmed-Alaa-2001 <63450270+Ahmed-Alaa-2001@users.noreply.github.com>
2022-08-04 20:12:36 +0200

    Update app.js

--- landing-page-project/js/app.js
+++ landing-page-project/js/app.js
@@ -1,10 +1,12 @@
 let sec=document.querySelectorAll("section");
 let navLst=document.querySelector("#nl");
+// Add class 'active' to section when near top of viewport
 window.onscroll=function(){
   for(i of sec){
       ((i.getBoundingClientRect().top>200||i.getBoundingClientRect().top<-200)?i.classList.remove("ac"):i.classList.add("ac"));
   }
 }
+// build the nav
 function navList(){
     sec=Array.from(sec);
     for(i of sec){
@@ -14,6 +16,7 @@ function navList(){
     }
    // console.log(navLst);
 }
+// Scroll to section on link click
 navLst.addEventListener("click", function(clk){
   clk.preventDefault();
     let getid=document.getElementById(`${clk.target.dataset.nav}`)
